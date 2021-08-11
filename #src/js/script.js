477f32874afbe0c53675d17528fee83f0392a9f2
minusvetal <minusvetal@gmail.com>
2021-08-11 15:11:32 +0300

    закончил слайдер + акция

--- #src/js/script.js
+++ #src/js/script.js
@@ -3,8 +3,9 @@
 
 $(document).ready(function(){
   $('.banner-slider__wrapper').slick({
-      autoplay: true,
-      arrows: true,
+    dots: true,
+    prevArrow: '<button class="banner-slider__btn banner-slider__btnprev"><svg class="icon icon-arrow"><use href="img/icons/icons.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button class="banner-slider__btn banner-slider__btnnext"><svg class="icon icon-arrow"><use href="img/icons/icons.svg#arrow-right"></use></svg></button>',
   });
 });
     
