8b8a673cd793caeeb8593c900f8122099b97e541
minusvetal <minusvetal@gmail.com>
2021-08-11 16:38:05 +0300

    end tabs-search

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,5 +1,6 @@
-@@include('webp.js');
-@@include('slick.min.js');
+// @ts-nocheck
+@@include('webp.js')
+@@include('slick.min.js')
 
 $(document).ready(function(){
   $('.banner-slider__wrapper').slick({
@@ -8,7 +9,14 @@ $(document).ready(function(){
     nextArrow: '<button class="banner-slider__btn banner-slider__btnnext"><svg class="icon icon-arrow"><use href="img/icons/icons.svg#arrow-right"></use></svg></button>',
   });
 });
-    
+
+$('.tabs-search__item').on('click', function (e) {
+  e.preventDefault();
+  $('.tabs-search__item').removeClass('tabs-search__item--active');
+  $('.tabs-search__content-item').removeClass('tabs-search__content-item--active');
+  $(this).addClass('tabs-search__item--active');
+  $($(this).attr('href')).addClass('tabs-search__content-item--active');
+})
 
 
 
