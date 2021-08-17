942b95f824527352b79cfd2d2f9c12a73946f305
minusvetal <minusvetal@gmail.com>
2021-08-17 16:29:15 +0300

    add banner  + slider POPULAR products

--- shop_0_to_1/js/script.js
+++ shop_0_to_1/js/script.js
@@ -43,22 +43,18 @@ $(document).ready(function(){
 $('.tab').on('click', function (e) {
   e.preventDefault();
 
-  $('.tab').removeClass('tab--active');
-  $('.tabs-content').removeClass('tabs-content--active');
+  $($(this).siblings()).removeClass('tab--active');
+  $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
   $(this).addClass('tab--active');
   $($(this).attr('href')).addClass('tabs-content--active');
 })
 
-$('.product-tab').on('click', function (e) {
-  e.preventDefault();
 
-  $('.product-tab').removeClass('product-tab--active');
-  $('.product-content').removeClass('product-content--active');
 
-  $(this).addClass('product-tab--active');
-  $($(this).attr('href')).addClass('product-content--active');
-})
+
+
+
 $('.products-item__favorite').on('click', function (e) {
   e.preventDefault();
   $(this).toggleClass('products-item__favorite--active')
